1b9c7aff7aeb3d09b613845c43e9bccf275feb26
Kevin Andres <kevinandresgom19@gmail.com>
2024-05-16 00:49:26 -0500

    feat: adjust styles

--- src/components/StartCard/StartCard.tsx
+++ src/components/StartCard/StartCard.tsx
@@ -9,17 +9,17 @@ const StartCard: React.FC<StartCardProps> = ({ onChange }) => {
   return (
     <div
       id="start"
-      className="flex flex-col items-center w-80 py-8 rounded-lg backdrop-blur-[8px]"
+      className="flex flex-col items-center w-80 py-8 rounded-2xl backdrop-blur-[8px] bg-white bg-opacity-15 shadow"
     >
       <p className="text-center font-semibold text-gray-300 ">
         Número de variables
       </p>
       <input
-        className="block mt-5 w-16 rounded py-2 px-3 font-semibold text-gray-200 bg-white bg-opacity-15"
+        className="block mt-5 w-16 rounded py-2 px-3 font-semibold text-gray-200 bg-white bg-opacity-25"
         type="number"
         onChange={onChange}
       />
-      <button className="mt-5 py-1 px-4 rounded-md font-semibold bg-white bg-opacity-30 transition-colors duration-200 hover:bg-opacity-60">
+      <button className="mt-5 py-1 px-4 rounded-md font-semibold bg-white bg-opacity-50 transition-colors duration-200 hover:bg-opacity-75">
         Empezar
       </button>
     </div>
