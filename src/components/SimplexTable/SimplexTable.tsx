afcefe5d1a6d305aa3d7afa22f2833e335293439
Kevin Andres <kevinandresgom19@gmail.com>
2024-06-02 21:46:08 -0500

    add initial table

--- src/components/SimplexTable/SimplexTable.tsx
+++ src/components/SimplexTable/SimplexTable.tsx
@@ -16,12 +16,14 @@ const SimplexTable: React.FC<SimplexTableProps> = ({
   standardizedObjectiveFunction,
   table,
 }) => {
+  const { basicVariables, standardizedConstraints, zj, cjzj } = table;
+
   const firstRow = (
     <>
       <Td isIdentifier border="r">
         <p className="min-w-max">{`Tabla ${numberOfTable}`}</p>
       </Td>
-      <Td semibold border="r">
+      <Td decoration border="r">
         <p>
           C<sub>j</sub>
         </p>
@@ -48,17 +50,17 @@ const SimplexTable: React.FC<SimplexTableProps> = ({
 
   const secondRow = (
     <>
-      <Td semibold border="r">
+      <Td decoration border="r">
         <p>
           C<sub>b</sub>
         </p>
       </Td>
-      <Td semibold border="r">
+      <Td decoration border="r">
         <p>Base</p>
       </Td>
       {standardizedObjectiveFunction.map((term) => {
         return (
-          <Td semibold border="r" key={[getRandomKey(), getRandomKey()].join("-")}>
+          <Td decoration border="r" key={[getRandomKey(), getRandomKey()].join("-")}>
             <p>
               {term.type}
               <sub>{term.subindex}</sub>
@@ -66,17 +68,114 @@ const SimplexTable: React.FC<SimplexTableProps> = ({
           </Td>
         );
       })}
-      <Td semibold>R</Td>
+      <Td decoration>R</Td>
+    </>
+  );
+
+  const constraintsRows = standardizedConstraints.map((standardizedConstraint, index) => {
+    const { coefficient, subindex, type } = basicVariables[index];
+    return (
+      <tr
+        className="border-b border-white border-opacity-30"
+        key={[getRandomKey(), getRandomKey].join("-")}
+      >
+        <Td border="r">
+          {type === EVariableType.ARTIFICIAL
+            ? coefficient === 1 || coefficient === -1
+              ? coefficient < 0
+                ? "-M"
+                : "M"
+              : `${coefficient}M`
+            : coefficient}
+        </Td>
+        <Td decoration border="r">
+          <p>
+            {type} <sub>{subindex}</sub>
+          </p>
+        </Td>
+        {standardizedConstraint.map((term, index) => {
+          const isLastIndex = index + 1 === standardizedConstraint.length;
+          return (
+            <Td
+              border={isLastIndex ? undefined : "r"}
+              key={[getRandomKey(), getRandomKey].join("-")}
+            >
+              {term.coefficient}
+            </Td>
+          );
+        })}
+      </tr>
+    );
+  });
+
+  const firstLastRow = (
+    <>
+      <Td border="r">{""}</Td>
+      <Td decoration border="r">
+        <p>
+          C<sub>j</sub>
+          {" - "}Z <sub>j</sub>
+        </p>
+      </Td>
+      {cjzj.map(({ value, artificialValue }) => {
+        const m =
+          artificialValue !== 0
+            ? artificialValue === 1 || artificialValue === -1
+              ? artificialValue < 0
+                ? "-M"
+                : "M"
+              : artificialValue + "M"
+            : "";
+
+        const c = value !== 0 ? value + "" : "";
+
+        return (
+          <Td border="r">
+            {c && m && !m.includes("-") ? c + "+" + m : !c && !m ? 0 : c + m}
+          </Td>
+        );
+      })}
+      <Td>{""}</Td>
+    </>
+  );
+
+  const secondLastRow = (
+    <>
+      <Td border="r">{""}</Td>
+      <Td decoration border="r">
+        <p>
+          Z<sub>j</sub>
+        </p>
+      </Td>
+      {zj.map(({ value, artificialValue }, index) => {
+        const isLastIndex = index + 1 === zj.length;
+        const m =
+          artificialValue !== 0
+            ? artificialValue === 1 || artificialValue === -1
+              ? artificialValue < 0
+                ? "-M"
+                : "M"
+              : artificialValue + "M"
+            : "";
+
+        const c = value !== 0 ? value + "" : "";
+
+        return (
+          <Td border={isLastIndex ? undefined : "r"}>
+            {c && m && !m.includes("-") ? c + "+" + m : !c && !m ? 0 : c + m}
+          </Td>
+        );
+      })}
     </>
   );
 
   return (
     <table className="block w-max mx-auto border border-white border-opacity-30 rounded-2xl backdrop-blur-[8px] bg-white bg-opacity-15 overflow-hidden shadow-sm">
-      <thead>
-        <tr className="border-b border-white border-opacity-30">{firstRow}</tr>
-        <tr>{secondRow}</tr>
-      </thead>
-      <tbody></tbody>
+      <tr className="border-b border-white border-opacity-30">{firstRow}</tr>
+      <tr className="border-b border-white border-opacity-30">{secondRow}</tr>
+      {constraintsRows}
+      <tr className="border-b border-white border-opacity-30">{secondLastRow}</tr>
+      <tr>{firstLastRow}</tr>
     </table>
   );
 };
--- src/components/Td/Td.tsx
+++ src/components/Td/Td.tsx
@@ -6,19 +6,27 @@ type BorderSide = "l" | "t" | "r" | "b";
 export interface TdProps {
   children: React.ReactNode;
   border?: BorderSide;
+  decoration?: boolean;
   isIdentifier?: boolean;
   rounded?: Rounded;
-  semibold?: boolean;
 }
 
-const Td: React.FC<TdProps> = ({ children, border, isIdentifier, rounded, semibold }) => {
+const Td: React.FC<TdProps> = ({
+  children,
+  border,
+  decoration,
+  isIdentifier,
+  rounded,
+}) => {
   return (
     <td
       className={`min-w-max py-2 px-3 text-center text-base text-white border-white border-opacity-30 ${
-        isIdentifier ? "bg-white bg-opacity-30" : ""
-      } ${semibold ? "font-semibold" : ""} ${border === "l" ? "border-l" : ""} ${
-        border === "t" ? "border-t" : ""
-      } ${border === "r" ? "border-r" : ""} ${border === "b" ? "border-b" : ""} 
+        isIdentifier ? "bg-blue-500 bg-opacity-80" : ""
+      } ${decoration ? "font-semibold bg-white bg-opacity-15" : ""} ${
+        border === "l" ? "border-l" : ""
+      } ${border === "t" ? "border-t" : ""} ${border === "r" ? "border-r" : ""} ${
+        border === "b" ? "border-b" : ""
+      } 
 				${rounded === "tl" ? "rounded-tl-2xl" : ""} ${rounded === "tr" ? "rounded-tr-2xl" : ""}
 				${rounded === "br" ? "rounded-br-2xl" : ""} ${rounded === "bl" ? "rounded-bl-2xl" : ""}
 			`}
